5d3cfda813d37122ff15fbd40fd9873841f13971
Ayush Rajput <akrrajput1@gmail.com>
2024-05-08 10:36:04 +0530

    new changes added

--- src/App1.js
+++ src/App1.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
-
+import { MdCancel } from "react-icons/md";
 class App1 extends Component {
     constructor(props)
     {
@@ -11,6 +11,7 @@ class App1 extends Component {
         };
         this.handleShow=this.handleShow.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleShow=this.handleShow.bind(this);
         
         setTimeout(()=>{
             console.log('constructor called after 4 seconds ');
@@ -27,11 +28,7 @@ class App1 extends Component {
         this.setState({show:true});
     }
 
-    handleShow=(ans)=>{
-
-        this.setState({show:ans});
-        this.setState({msg:''})
-    }
+    
     
     componentDidMount(){
         
@@ -54,16 +51,34 @@ class App1 extends Component {
         console.log('prevState--->',prevState.msg)
       }
 
+      handleShow(){
+        console.log('clicked on it');
+        this.setState({show:false})
+        this.setState({msg:''})
+
+
+        
+    }
+
+    componentWillUnmount()
+    {
+        console.log('component unmounted immediately');
+    }
+    
+
     render() {
         return (
             <div className='flex  bg-gray-400 relative h-[100vh] items-center justify-center '>
                 <div className='flex gap-2  shaodw shadow-white  px-5 py-4 rounded-md bg-orange-600'>
-                    <input type="text" className='px-2 py-1 rounded-sm focus:border-none focus:outline-none' name={this.state.msg} id="" value={this.state.msg} onChange={this.handleChange} />
+                    <input type="text" disabled={this.state.show?true :false} className='px-2 py-1 rounded-sm focus:border-none focus:outline-none' name={this.state.msg} id="" value={this.state.msg} onChange={this.handleChange} />
                     <button className='border border-white px-2 rounded-sm text-white hover:bg-blue-500' onClick={this.onSubmit}>Show Me</button>
                    
                 </div>
                 {
-                    this.state.show && <Modal data={this.state.msg} handleShow={this.handleShow}/>
+                    this.state.show &&  <div className='absolute h-[150px] w-[400px] border bg-neutral-600 px-4 py-3 flex justify-center items-center text-white text-3xl rounded-sm left-[45%] border-gray-200 top-3 '>
+                                            <div>{this.state.msg}</div>
+                                            <MdCancel size={20} className='absolute right-2 top-2' onClick={this.handleShow}/>
+                                        </div>
                 }
             </div>
         );
